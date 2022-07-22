b27361f4254c4999b1ef32b26023f685b7f28b50
Cimorexave <sadeq.s502@gmail.com>
2022-07-22 15:41:32 +0430

    added routing

--- react-challenge/src/App.tsx
+++ react-challenge/src/App.tsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { TextField, InputAdornment } from '@mui/material';
+import { TextField, InputAdornment, Switch } from '@mui/material';
 import './styles/App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import UserInfo from './pages/UserInfo';
 
 function App() {
   const [username, setUsername] = useState<String>("")
@@ -14,6 +16,8 @@ function App() {
   return (
     <BrowserRouter>
       <div className="app">
+          <Route path='/' element={<Home />} />
+          <Route path='/User' element={<UserInfo />} />
         <TextField 
         onChange={handleChange}
         id='textInput'
