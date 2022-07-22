595f49b2ca7d5e6344781307d3a2c18e8aca3eac
Cimorexave <sadeq.s502@gmail.com>
2022-07-22 15:07:53 +0430

    added basic structure and styling

--- react-challenge/src/App.tsx
+++ react-challenge/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { TextField, InputAdornment } from '@mui/material';
+import './styles/App.css';
 
 function App() {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value)
+  }
   return (
-    <>
-    
-    </>
+    <div className="app">
+      <TextField 
+      onChange={handleChange}
+      id='textInput'
+      variant='outlined'
+      label='Search'
+      placeholder='Username...'
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <GitHubIcon fontSize='medium' color='primary'/>
+          </InputAdornment>
+        ),
+      }}
+      />
+    </div>
   );
 }
 
