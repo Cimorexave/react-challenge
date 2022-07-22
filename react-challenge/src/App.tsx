5db386c268e721960b957f4c2a91e3a48469cb6c
Cimorexave <sadeq.s502@gmail.com>
2022-07-22 15:23:16 +0430

    updated component; added react router

--- react-challenge/src/App.tsx
+++ react-challenge/src/App.tsx
@@ -2,29 +2,35 @@ import React, {useState, useEffect} from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { TextField, InputAdornment } from '@mui/material';
 import './styles/App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const [username, setUsername] = useState<String>("")
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.currentTarget.value)
+    if (e.currentTarget.value.length > 3) setUsername(e.currentTarget.value)
   }
   return (
-    <div className="app">
-      <TextField 
-      onChange={handleChange}
-      id='textInput'
-      variant='outlined'
-      label='Search'
-      placeholder='Username...'
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <GitHubIcon fontSize='medium' color='primary'/>
-          </InputAdornment>
-        ),
-      }}
-      />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <TextField 
+        onChange={handleChange}
+        id='textInput'
+        variant='outlined'
+        label='Search'
+        placeholder='Username...'
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <GitHubIcon fontSize='medium' color='primary'/>
+            </InputAdornment>
+          ),
+        }}
+        />
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
