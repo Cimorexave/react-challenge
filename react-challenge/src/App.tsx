05e38818d5ab074482e956946562874ec80a1376
Cimorexave <sadeq.s502@gmail.com>
2022-07-22 15:52:03 +0430

    managed components of home page

--- react-challenge/src/App.tsx
+++ react-challenge/src/App.tsx
@@ -1,37 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { TextField, InputAdornment, Switch } from '@mui/material';
-import './styles/App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import UserInfo from './pages/UserInfo';
 
 function App() {
-  const [username, setUsername] = useState<String>("")
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value)
-    if (e.currentTarget.value.length > 3) setUsername(e.currentTarget.value)
-  }
+  
   return (
     <BrowserRouter>
-      <div className="app">
-          <Route path='/' element={<Home />} />
-          <Route path='/User' element={<UserInfo />} />
-        <TextField 
-        onChange={handleChange}
-        id='textInput'
-        variant='outlined'
-        label='Search'
-        placeholder='Username...'
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <GitHubIcon fontSize='medium' color='primary'/>
-            </InputAdornment>
-          ),
-        }}
-        />
+      <div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/:username' element={<UserInfo />} />
+          </Routes>
       </div>
     </BrowserRouter>
     
--- react-challenge/src/pages/Home.tsx
+++ react-challenge/src/pages/Home.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { TextField, InputAdornment } from '@mui/material';
+import '../styles/Home.css';
 const Home = () => {
+    const [username, setUsername] = useState<String>("")
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.currentTarget.value)
+        if (e.currentTarget.value.length > 3) setUsername(e.currentTarget.value)
+      }
+
+    
   return (
-    <div>Home</div>
+    <div className='home'>
+        <TextField 
+        onChange={handleChange}
+        id='textInput'
+        variant='outlined'
+        label='Search'
+        placeholder='Username...'
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <GitHubIcon fontSize='medium' color='primary'/>
+            </InputAdornment>
+          ),
+        }}
+        />
+      </div>
   )
 }
 
