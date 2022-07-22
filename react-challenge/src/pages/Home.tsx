e78479ad5a5a3ebb02179a91fab016b5d3cea3ff
Cimorexave <sadeq.s502@gmail.com>
2022-07-22 17:20:33 +0430

    bug fixed: changed onChange to onChangeCapture

--- react-challenge/src/pages/Home.tsx
+++ react-challenge/src/pages/Home.tsx
@@ -1,24 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { TextField, InputAdornment } from '@mui/material';
 import '../styles/Home.css';
+import {DebounceInput} from 'react-debounce-input';
+
 
 const Home = () => {
     const [username, setUsername] = useState<String>("")
+    const [userData, setUserData] = useState()
 
+    useEffect(() => {
+        if (username.length > 3) {
+            fetch(`https://api.github.com/users/${username}`)
+                .then(response => {
+                    console.log(response)
+                    if (!response.ok) {console.error(Error)}
+                    response.json().then (data => {
+                        console.log(data)
+
+                    })
+                }
+                    
+                )
+        }
+    }, [username])
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.currentTarget.value)
-        if (e.currentTarget.value.length > 3) setUsername(e.currentTarget.value)
+        console.log(e.target.value)
+        setUsername(e.target.value)
       }
 
   return (
     <div className='home'>
-        <TextField 
-        onChange={handleChange}
+        <TextField
         id='textInput'
         variant='outlined'
         label='Search'
         placeholder='Username...'
+        onChangeCapture={handleChange}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
