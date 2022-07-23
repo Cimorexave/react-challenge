63abfda62c4f1a7210e0feae879143e4f55a1093
Cimorexave <sadeq.s502@gmail.com>
2022-07-23 20:25:06 +0430

    removed redux state

--- react-challenge/src/state/combinedReducers.ts
+++
@@ -1,9 +0,0 @@
-import reducer from './reducers/userReducer';
-import { combineReducers } from 'redux';
-
-const reducers = combineReducers({
-    user: reducer
-})
-
-export default reducers
-export type State = ReturnType <typeof reducers>
--- react-challenge/src/state/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware} from 'redux';
-import reducers from './combinedReducers';
-import thunk from 'redux-thunk'
-export const store = createStore(
-    reducers, 
-    {},
-    applyMiddleware(thunk)
-)
