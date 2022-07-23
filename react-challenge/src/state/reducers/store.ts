d735feaf5885fde60918674dd6c8c6e193068655
Cimorexave <sadeq.s502@gmail.com>
2022-07-23 19:33:54 +0430

    added redux store and redux thunk

---
+++ react-challenge/src/state/reducers/store.ts
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware} from 'redux';
+import reducers from '../combinedReducers';
+import thunk from 'redux-thunk'
+export const store = createStore(
+    reducers, 
+    {},
+    applyMiddleware(thunk)
+)
