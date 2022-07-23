ebf3684f9418499f560b0cf087126987df536161
Cimorexave <sadeq.s502@gmail.com>
2022-07-23 20:34:39 +0430

    wrapped everything in the app in the userContext

--- react-challenge/src/App.tsx
+++ react-challenge/src/App.tsx
@@ -4,13 +4,11 @@ import Home from './pages/Home';
 import UserInfo from './pages/UserInfo';
 import MostPopular from './pages/MostPopular';
 
-
 function App() {
   
   return (
     <BrowserRouter>
       <>
-        
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/most-popular' element={<MostPopular />}/>
--- react-challenge/src/index.tsx
+++ react-challenge/src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {userContext} from './Helper/context';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <userContext.Provider>
     <App />
+  </userContext.Provider>
 );
