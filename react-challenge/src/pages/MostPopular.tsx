85abc16385e7e2e24d095ba5acf52844b8300c16
Cimorexave <sadeq.s502@gmail.com>
2022-07-24 16:49:32 +0430

    setting new page component

--- react-challenge/src/pages/MostPopular.tsx
+++ react-challenge/src/pages/MostPopular.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
-
+import React , {useContext, useState, useEffect} from 'react';
+import { userContext } from '../Helper/context';
 const MostPopular = () => {
+  const [userData, setUserData] = useContext(userContext)
+  const [reposListState, setReposListState] = useState([])
+  useEffect(() => {
+    fetch(userData.repos_url).then(response => {
+      if (response.ok) {
+        response.json().then(repoData => {
+          console.log(repoData)
+          setReposListState(repoData)
+        })
+      } else console.error("Request Failed fetching data; try again.")
+    })
+  })
   return (
     <div>MostPopular</div>
   )
