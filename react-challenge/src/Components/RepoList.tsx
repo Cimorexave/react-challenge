e39e17560b2ae974e3836522243db3e3335e2d9f
Cimorexave <sadeq.s502@gmail.com>
2022-07-24 14:45:24 +0430

    added repolist component

---
+++ react-challenge/src/Components/RepoList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const RepoList: React.FC<any> = ()  => {
+    // Values: Name, Stars, Forks, Last Update, Language, Bio
+  return (
+    <div>RepoList</div>
+  )
+}
+
+export default RepoList
--- react-challenge/src/pages/UserInfo.tsx
+++ react-challenge/src/pages/UserInfo.tsx
@@ -1,10 +1,25 @@
-import React, { ReactComponentElement, useContext } from 'react'
-import { userContext } from '../Helper/context'
+import React, { ReactComponentElement, useContext, useEffect, useState } from 'react'
+import { userContext } from '../Helper/context';
+import RepoList from '../Components/RepoList';
 
 const UserInfo = () => {
 
   const {userData, setUserData} = useContext(userContext)
+  const [userDataRepos , setUserDataRepos] = useState()
   console.log(userData)
+
+  useEffect(() => {
+    if (userData.repos_url) {
+      fetch(userData.repos_url).then(response => {
+        if (response.ok) {
+          response.json().then(repoData => {
+            console.log(repoData)
+            setUserDataRepos(repoData)
+          })
+        }
+      })
+    }
+  },[userData])
 
   return (
     <>
@@ -12,6 +27,11 @@ const UserInfo = () => {
         <p>{userData.location}</p>
         <img src={userData.avatar_url} alt="avatar" />
         <p>public repos: {userData.public_repos}</p>
+        {/*the repository list*/}
+        <div>{userData.repos_url}</div>
+        <RepoList 
+        reposList ={userDataRepos}
+        />
     </>
   )
 }
