54e535b7b11ff26170959c878d80cecf77ffc43c
Cimorexave <sadeq.s502@gmail.com>
2022-07-25 17:56:56 +0430

    updated styling

--- react-challenge/src/pages/Home.tsx
+++ react-challenge/src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React, {useState, useEffect, useContext} from 'react';
 import { userContext } from '../Helper/context';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { TextField, InputAdornment, Button } from '@mui/material';
+import { TextField, InputAdornment, Button, Typography } from '@mui/material';
 import '../styles/Home.css';
 import { useNavigate } from 'react-router-dom';
+import CheckIcon from '@mui/icons-material/Check';
+//importing mui robot typography
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const Home = () => {
 
@@ -13,24 +20,31 @@ const Home = () => {
     const [username, setUsername] = useState<String>("")
     const [userExists, setUserExists] = useState<boolean>(false)
     const {userData, setUserData} = useContext(userContext)
+/*
+    useEffect(() => {
+      if (userExists) {
+        document.getElementById('checkIcon').style.color = "green"
+      }
+    }, [userExists]) */
 
     useEffect(() => {
-        if (username.length > 3) {
+            document.getElementById('checkIcon').style.color = "white"
             fetch(`https://api.github.com/users/${username}`)
                 .then(response => {
-                    console.log(response)
-                    if (!response.ok) {console.error(Error)}
-                    response.json().then (data => {
-                        console.log(data)
+                    console.log(response.ok)
+                    if (!response.ok) {console.error(Error); setUserExists(false);}
+                    else {response.json().then (data => {
+                        //console.log(data)
                         setUserExists(true)
+                        document.getElementById('checkIcon').style.color = "green"
                         setUserData(data)
                         //if (data.login)
                           //navigate(`/${data.login}`, {replace: true})
                     })
+                  }
                 }
                     
                 )
-        }
     }, [username])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,24 +58,33 @@ const Home = () => {
     }
   return (
     <div className='home'>
-        <TextField
-        value={username}
-        id='textInput'
-        variant='outlined'
-        label='Search'
-        placeholder='Username...'
-        onChangeCapture={handleChange}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <GitHubIcon fontSize='medium' color='primary'/>
-            </InputAdornment>
-          ),
-        }}
-        />
-        <Button variant="contained" size="medium" onClick={handleClick}
-        > Search </Button>
-        
+        <Typography variant='h6' >Search User and click the button</Typography>
+        <div className="searchField">
+          <TextField
+          sx = {{padding: "0.5em", width: "18em" }}
+          value={username}
+          id='textInput'
+          variant='outlined'
+          label='Search'
+          placeholder='Search Here...'
+          onChangeCapture={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <GitHubIcon fontSize='medium' color='primary'/>
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position='end'>
+                <CheckIcon fontSize="small" id="checkIcon"/>
+              </InputAdornment>
+            )
+          }}
+          />
+          <Button variant="contained" size="medium" onClick={handleClick}
+          sx={{fontSize: 'smaller', height: "3em", width: "7em"}}  > Search </Button>
+        </div>
+      
       </div>
   )
 }
