04fbf28a9433b191e1f31ba60abe5a3c514ef94d
Cimorexave <sadeq.s502@gmail.com>
2022-07-25 21:10:44 +0430

    completed styling of component

--- react-challenge/src/Components/RepoList.jsx
+++ react-challenge/src/Components/RepoList.jsx
@@ -1,5 +1,11 @@
-import React, {useState, useEffect, useRef} from 'react'
-//(props: any) => { props: any; "": any; }
+import { Button, Typography } from '@mui/material';
+import React, {useState, useEffect} from 'react';
+import { Card, CardContent, CardMedia, Breadcrumbs, Chip } from '@mui/material';
+import ForkRightIcon from '@mui/icons-material/ForkRight';
+import StarIcon from '@mui/icons-material/Star';
+import '../styles/RepoList.css'
+
+
 const RepoList = ( {reposList} )  => {
   const [reposListState, setReposListState] = useState([])
   const [sortChanged, setSortChanged] = useState(0)
@@ -38,27 +44,46 @@ const RepoList = ( {reposList} )  => {
   return (
     <>
       <div className="listContainer">
-        {/*handling the buttons with sort method on the repoListState*/}
-        <button onClick={sortByStars} >Sort by stars</button>
-        <button onClick={sortByForks} >Sort by forks</button>
-        <button onClick={sortByTime} >Sort by time</button>
-        Repositories: 
-        {
+        <div className="heading">
+          {/*handling the buttons with sort method on the repoListState*/}
+          <Typography variant='h5' > Repositories </Typography>
+          <div className="buttons">
+            <Button variant='contained' size='small' onClick={sortByStars} >Sort by stars</Button>
+            <Button variant='contained' size='small' onClick={sortByForks} >Sort by Forks</Button>
+            <Button variant='contained' size='small' onClick={sortByTime} >Sort by Time</Button>
+          </div>
+        </div>
         
+        <div className="list">
+          
+        {
         reposListState.map( repo => {
           return (
-            <div className='repoCard' key={repo.id}>
-              
-                <h1>{repo.name}</h1>
-                <p>{repo.language}</p>
-                {repo.private ? <p>private</p> : <p>public</p> }
-                <p>forks: {repo.forks}</p>
-                <p>stars: {repo.stargazers_count}</p>
-                <p>Last updated at: {new Date(repo.updated_at).toString().slice(0, 25)}</p>
+            <Card key={repo.id} sx={{display: "flex", flexDirection: "column",
+            padding: "1em 3em", backgroundColor: "#cae3d7",
+           borderRadius: 15, justifyContent: "center", alignItems: "center", textAlign: "center" }} >
+            <div className="content">
+            <Typography variant="h3" >{repo.name}</Typography>
+            <CardContent sx={{ display: 'flex', margin: 0, flexDirection: "column", gap: 2 }} >
+              <Chip label={`${repo.language}`} variant="outlined" sx={{ width: "fit-content", alignSelf: "center" }} />
+            
+              <div style={{ display: "flex", gap: "0.75em", justifyContent: "center" }} >
+                <Chip label={`Stars: ${repo.stargazers_count}`} icon={<StarIcon />} variant="filled" color="primary" />
+                <Chip label={`Forks: ${repo.forks}`} icon={<ForkRightIcon />} variant="filled" color='secondary' />
               </div>
+              
+              <Typography variant='caption' >Last updated at: {new Date(repo.updated_at).toString().slice(0, 25)} </Typography>
+            </CardContent>
+            </div>
+            
+          </Card>
+            
             )
           } )
         }
+
+        </div>  
+        
       </div>
       
     </>
